0ed1d0434a87695d0db335d5ad6a3c9ebb50309a
Tomas <tomasszabo94@gmail.com>
2025-03-01 20:13:16 -0300

    added error handling for mongoose, up on authentication now

--- app.js
+++ app.js
@@ -5,14 +5,25 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';   
 import { connect } from 'mongoose';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+    cookieParser()  
+)
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
+
 
 // webpage message
 app.get('/', (req, res) => {
